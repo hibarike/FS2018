a52b7f32da543254bb7c7093176080243700d8fd
Vitaliy Derkach <43149743+vitderkach@users.noreply.github.com>
2018-09-23 19:04:08 +0300

    Add Program.cs

---
+++ Vitaliy Derkach/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+[assembly: CLSCompliant(true)]
+namespace Calc
+{
+    class Program
+    {
+        static void Main()
+        {
+            Calc c = new Calc();
+            ulong ans = c.Add(10, 84);
+            Console.WriteLine("10 + 84 is: {0}", ans);
+            Console.ReadLine();
+        }
+    }
+
+    class Calc
+    {
+        public ulong Add(int x, int y) {
+            return (ulong)(x + y);
+        }
+    }
+}
