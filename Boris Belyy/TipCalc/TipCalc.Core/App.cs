e552e7febc9d2469806b56643bbbc6c1b8a9bc9b
Boris Belyy <hibarike@gmail.com>
2018-10-29 19:24:04 +0200

    TipCalc.Core

---
+++ Boris Belyy/TipCalc/TipCalc.Core/App.cs
@@ -0,0 +1,17 @@
+﻿using MvvmCross;
+using MvvmCross.ViewModels;
+using TipCalc.Core.Services;
+using TipCalc.Core.ViewModels;
+
+namespace TipCalc.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.RegisterType<ICalculationService, CalculationService>();
+
+            RegisterAppStart<TipViewModel>();
+        }
+    }
+}
---
+++ Boris Belyy/TipCalc/TipCalc.Core/Services/ICalculationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipCalc.Core.Services
+{
+    interface ICalculationService
+    {
+        double TipAmount(double subTotal, int generosity);
+
+    }
+}
