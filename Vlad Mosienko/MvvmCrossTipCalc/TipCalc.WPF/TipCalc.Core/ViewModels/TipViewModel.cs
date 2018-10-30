66829e05f4e31e8a7e9667ae240645b71b5f44c8
vlad_mosienko <mosienko20133@gmail.com>
2018-10-30 21:37:07 +0200

    Implement the Model. Implement a ViewModel.

---
+++ Vlad Mosienko/MvvmCrossTipCalc/TipCalc.WPF/TipCalc.Core/ViewModels/TipViewModel.cs
@@ -0,0 +1,68 @@
+﻿using MvvmCross.ViewModels;
+using TipCalc.Core.Services;
+using System.Threading.Tasks;
+
+namespace TipCalc.Core.ViewModels
+{
+    public class TipViewModel : MvxViewModel
+    {
+        readonly ICalculationService _calculationService;
+
+        public TipViewModel(ICalculationService calculationService)
+        {
+            _calculationService = calculationService;
+        }
+
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+
+            _subTotal = 100;
+            _generosity = 10;
+
+            Recalculate();
+        }
+
+        private double _subTotal;
+        public double SubTotal
+        {
+            get => _subTotal;
+            set
+            {
+                _subTotal = value;
+                RaisePropertyChanged(() => SubTotal);
+
+                Recalculate();
+            }
+        }
+
+        private int _generosity;
+        public int Generosity
+        {
+            get => _generosity;
+            set
+            {
+                _generosity = value;
+                RaisePropertyChanged(() => Generosity);
+
+                Recalculate();
+            }
+        }
+
+        private double _tip;
+        public double Tip
+        {
+            get => _tip;
+            set
+            {
+                _tip = value;
+                RaisePropertyChanged(() => Tip);
+            }
+        }
+
+        private void Recalculate()
+        {
+            Tip = _calculationService.TipAmount(SubTotal, Generosity);
+        }
+    }
+}
