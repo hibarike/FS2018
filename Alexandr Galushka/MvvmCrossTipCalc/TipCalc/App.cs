09e118152dea3796b97bcb169b3b058fda93c7af
Alexandr Galushka <galushka.alex.1998@gmail.com>
2018-10-30 21:50:23 +0200

    no message

---
+++ Alexandr Galushka/MvvmCrossTipCalc/TipCalc/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TipCalc.Core.Services;
+using TipCalc.Core.ViewModels;
+using MvvmCross;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+
+namespace TipCalc.Core
+{
+    class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.RegisterType<ICalculationService, CalculationService>();
+            RegisterAppStart<TipViewModel>();
+        }
+    }
+}
