1d44a68a81db45dacc66ede9d2fc0b6662afa78c
vlad_mosienko <mosienko20133@gmail.com>
2018-10-30 21:20:47 +0200

    Model designing

---
+++ Vlad Mosienko/MvvmCrossTipCalc/TipCalc.WPF/TipCalc.Core/Services/ICalculationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipCalc.Core.Services
+{
+    public interface ICalculationService
+    {
+        double TipAmount(double subTotal, int generosity);
+    }
+}
