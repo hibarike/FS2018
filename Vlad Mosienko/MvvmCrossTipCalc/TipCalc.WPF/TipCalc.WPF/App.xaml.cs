d46d9cd5e40c626b908c32fe613c441118db60e6
vlad_mosienko <mosienko20133@gmail.com>
2018-10-30 21:14:17 +0200

    Creating interface and a View.

---
+++ Vlad Mosienko/MvvmCrossTipCalc/TipCalc.WPF/TipCalc.WPF/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using MvvmCross.Core;
+using MvvmCross.Platforms.Wpf.Core;
+using MvvmCross.Platforms.Wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TipCalc.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : MvxApplication
+    {
+        protected override void RegisterSetup()
+        {
+            this.RegisterSetupType<MvxWpfSetup<Core.App>>();
+        }
+    }
+}
