1a6dfd2780aa972d51770dac9ec4b0d8fdd4a4b6
AlexSirota <sirotaalexandr30@gmail.com>
2018-11-06 23:03:46 +0200

    Added a Model

---
+++ Alexandr Sirota/MvvmCrossTipCalc/TipCalc/TipCalc/Services/ICalculationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//used for calculating tips
+
+namespace TipCalc.Services
+{
+    public interface ICalculationService
+    {
+        double TipAmount(double subTotal, int generosity);
+    }
+}
