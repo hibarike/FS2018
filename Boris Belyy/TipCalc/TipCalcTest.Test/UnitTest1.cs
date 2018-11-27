8cb769484f6ddea54dd6aee18074160a0e0bdfbb
Boris Belyy <hibarike@gmail.com>
2018-11-27 17:07:17 +0200

    Making MvvmCross task - UnitTest

---
+++ Boris Belyy/TipCalc/TipCalcTest.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TipCalc.Core.Services;
+
+namespace TipCalcTest.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+      
+            public void TestThatZeroGenerosityReturnZeroTip()
+            {
+                //Arrange
+                var tip = new CalculationService();
+                //Act
+                var result = tip.TipAmount(42.35, 0);
+                //Assert
+                Assert.AreEqual(0, result);
+            }
+
+        }
+    }
