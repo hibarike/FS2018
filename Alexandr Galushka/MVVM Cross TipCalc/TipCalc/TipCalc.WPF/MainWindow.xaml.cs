4b2169949110b56062c33de214e61348c78f0fd0
Alexandr <galushka.alex.1998@gmail.com>
2018-12-01 16:27:37 +0300

    Add a View (WPF)

---
+++ Alexandr Galushka/MVVM Cross TipCalc/TipCalc/TipCalc.WPF/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace TipCalc.WPF
+{
+    public partial class MainWindow : MvxWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
