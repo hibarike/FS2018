ee26c526ecd85b8546206c48fdd98aa8788551a7
Vitaliy Derkach <alex.chernyy10@gmail.com>
2018-12-02 20:43:55 +0200

    Add interface and a view.

---
+++ Vitaliy Derkach/MvvmCrossTipCalc/TipCalc/TipCalc.Core/App.cs
@@ -0,0 +1,17 @@
+﻿using MvvmCross;
+using MvvmCross.ViewModels;
+using TipCalc.Core.Services;
+using TipCalc.Core.ViewModels;
+
+namespace TipCalc.Core
+{
+	class App: MvxApplication
+	{
+		public override void Initialize()
+		{
+			Mvx.RegisterType<ICalculationService, CalculationService>();
+
+			RegisterAppStart<TipViewModel>();
+		}
+	}
+}
